4cddcc0aff78da90eccc9010a91cafaa4ab894f8
vihaan1245 <96100376+vihaan1245@users.noreply.github.com>
2022-04-14 06:26:34 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas = createCanvas(550,550);
+    canvas.position(560,100);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('Posenet is not initialized, congrats on failing!');
+}
+
+function draw()
+{
+    background('#C0FF00');
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+      console.log(results);
+  }
+}
